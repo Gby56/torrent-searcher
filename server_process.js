d1121087f8a6e6037f776fd7e7f5908cc64ad0a6
Stanislas Daniel Claude <stanislas.dolcini@gmail.com>
2017-03-24 02:05:11 +0100

    Make it functionnal

--- server_process.js
+++ server_process.js
@@ -6,7 +6,7 @@
  * , copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
 
@@ -14,19 +14,19 @@
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT
  * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **/
 
 "use strict";
 let Promise = require('promise');
-let g_research_term = "Tokyo Drift";
 /**
  * Default port used by application.
  */
 const g_serverVariables = {
-    "port": process.env.PORT,
-    "ip": process.env.IP
+    "port": 2000,
+    "ip": process.env.IP,
+    "search_keywords": "tokyo"
 };
 
 
@@ -39,28 +39,32 @@ let PirateBay = require('thepiratebay')
 
 let TorrentSearchApi = require('torrent-search-api');
 let torrentSearch = new TorrentSearchApi();
-
 let express = require('express');
 let app = express();
-
-
-app.listen(g_serverVariables.port, g_serverVariables.ip);
+var bodyParser = require('body-parser')
+let server = require('http').Server(app);
+server.listen(g_serverVariables.port);
+//app.listen(g_serverVariables.port, g_serverVariables.ip);
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 });
 app.get('/index', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 });
-
-
+app.get('/form_action', (request, response) => {
+    response.sendFile(__dirname + '/form_action.js')
+});
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // ----------------------- EXTRATORRENTS -----------------------
+/*  console.log("Starting query for: " + search_keywords)
 let ExtraTorrentPromise = Promise.resolve(extraTorrentAPI.search({
-        with_words: g_research_term,
-    })
-    .then(results => {
+        "with_words": search_keywords
+    }).then(result => {
         let clean = [];
-        for (let r of results.results) {
+        for (let r of result.results) {
             clean.push({
                 "title": r.title,
                 "url": r.url,
@@ -71,101 +75,173 @@ let ExtraTorrentPromise = Promise.resolve(extraTorrentAPI.search({
         }
         return clean;
     })
-    .catch(err => {
-        return err
-    })
+    .catch(err => console.log(err))
 );
+*/
 
-// ----------------------- THEPIRATEBAY -----------------------
-let PirateBayPromise = Promise.resolve(PirateBay.search(g_research_term, {
-        category: 'all', // default - 'all' | 'all', 'audio', 'video', 'xxx', 
-        //                   'applications', 'games', 'other' 
-        // 
-        // You can also use the category number: 
-        // `/search/0/99/{category_number}` 
-        filter: {
-            verified: false // default - false | Filter all VIP or trusted torrents 
-        },
-        page: 0, // default - 0 - 99 
-        orderBy: 'leeches', // default - name, date, size, seeds, leeches 
-        sortBy: 'desc' // default - desc, asc 
-    })
-    .then(results => {
-        let clean = [];
-        for (let r of results) {
-            clean.push({
-                "title": r.name,
-                "url": r.link,
-                "size": r.size,
-                "seeds": r.seeders,
-                "leechers": r.leechers
-            });
-        }
-        return clean;
-    })
-    .catch(err => {
-        return err
-    })
-);
+let performQuery = function(search_keywords) {
+    // ----------------------- THEPIRATEBAY -----------------------
+    let PirateBayPromise = Promise.resolve(PirateBay.search(search_keywords, {
+            category: 'all', // default - 'all' | 'all', 'audio', 'video', 'xxx',
+            //                   'applications', 'games', 'other'
+            //
+            // You can also use the category number:
+            // `/search/0/99/{category_number}`
+            filter: {
+                verified: false // default - false | Filter all VIP or trusted torrents
+            },
+            page: 0, // default - 0 - 99
+            orderBy: 'leeches', // default - name, date, size, seeds, leeches
+            sortBy: 'desc' // default - desc, asc
+        })
+        .then(results => {
+            let clean = [];
+            for (let r of results) {
+                clean.push({
+                    "title": r.name,
+                    "url": r.link,
+                    "size": r.size,
+                    "seeds": r.seeders,
+                    "leechers": r.leechers
+                });
+            }
+            return clean;
+        })
+        .catch(err => {
+            return err
+        })
+    );
 
-// ----------------------- TORRENTSEARCH -----------------------
-torrentSearch.enableProvider('1337x');
-torrentSearch.enableProvider('Torrent9');
-torrentSearch.enableProvider('Torrentz2');
-torrentSearch.enableProvider('ZeTorrents');
+    // ----------------------- TORRENTSEARCH -----------------------
+    torrentSearch.enableProvider('1337x');
+    torrentSearch.enableProvider('Torrent9');
+    torrentSearch.enableProvider('Torrentz2');
+    torrentSearch.enableProvider('ZeTorrents');
 
-// Search '1080' in 'Movies' category and limit to 20 results 
-let torrentSearchPromise = Promise.resolve(
-    torrentSearch.search(g_research_term, 'all', 20)
-    .then(results => {
-        let clean = [];
-        for (let r of results) {
-            clean.push({
-                "title": r.title,
-                "url": r.desc,
-                "size": r.size,
-                "seeds": r.seeds,
-                "leechers": (!(isNaN(r.peers - r.seed)) &&
-                        (r.peers - r.seed > 0)) ?
-                    r.peers - r.seed : 0
-            });
-        }
-        return clean;
-    })
-    .catch(err => {
-        return err;
-    })
-);
+    // Search '1080' in 'Movies' category and limit to 20 results
+    let torrentSearchPromise = Promise.resolve(
+        torrentSearch.search(search_keywords, 'all', 20)
+        .then(results => {
+            let clean = [];
+            for (let r of results) {
+                clean.push({
+                    "title": r.title,
+                    "url": r.desc,
+                    "size": r.size,
+                    "seeds": r.seeds,
+                    "leechers": (!(isNaN(r.peers - r.seed)) &&
+                            (r.peers - r.seed > 0)) ?
+                        r.peers - r.seed : 0
+                });
+            }
+            return clean;
+        })
+        .catch(err => {
+            return err;
+        })
+    );
 
-Promise.all([torrentSearchPromise, PirateBayPromise, ExtraTorrentPromise]).then(values => {
+    Promise.all([torrentSearchPromise, PirateBayPromise /*, ExtraTorrentPromise*/ ]).then(values => {
         let resultArray = [];
         let torrentTitleList = [];
-
-        for (let [key, value] of Object.entries(values)) {
-            console.log(key, value);
+        console.log("size:" + values.length);
+        for (let i = 0; i < values.length; ++i) {
+            if (values[i] !== [] && values[i] !== undefined) {
+                for (let torrent of values[i]) {
+                    if (!torrentTitleList.some(title => title === torrent.title)) {
+                        torrentTitleList.push(torrent.title);
+                        resultArray.push(torrent);
+                    }
+                }
+            } else {
+                console.log("error in value " + i);
+            }
         }
+        console.log(resultArray);
+        console.log(resultArray.length + " result(s) found.");
+    }).catch(err => console.log(err));
+}
+app.post('/', (request, response) => {
+    g_serverVariables.search_keywords = request.body.search_keywords;
+    console.log("In POST: " + request.body.search_keywords);
+});
+app.get('/getData', (req, res) => {
+    console.log("In GET: " + g_serverVariables.search_keywords);
+    performQuery(g_serverVariables.search_keywords);
+    // ----------------------- THEPIRATEBAY -----------------------
+    let PirateBayPromise = Promise.resolve(PirateBay.search(g_serverVariables.search_keywords, {
+            category: 'all', // default - 'all' | 'all', 'audio', 'video', 'xxx',
+            //                   'applications', 'games', 'other'
+            //
+            // You can also use the category number:
+            // `/search/0/99/{category_number}`
+            filter: {
+                verified: false // default - false | Filter all VIP or trusted torrents
+            },
+            page: 0, // default - 0 - 99
+            orderBy: 'leeches', // default - name, date, size, seeds, leeches
+            sortBy: 'desc' // default - desc, asc
+        })
+        .then(results => {
+            let clean = [];
+            for (let r of results) {
+                clean.push({
+                    "title": r.name,
+                    "url": r.link,
+                    "size": r.size,
+                    "seeds": r.seeders,
+                    "leechers": r.leechers
+                });
+            }
+            return clean;
+        })
+        .catch(err => {
+            return err
+        })
+    );
 
-        // for (let i = 0; i < values.length; ++i) {
+    // ----------------------- TORRENTSEARCH -----------------------
+    torrentSearch.enableProvider('1337x');
+    torrentSearch.enableProvider('Torrent9');
+    torrentSearch.enableProvider('Torrentz2');
+    torrentSearch.enableProvider('ZeTorrents');
 
-        //     if (values[i] !== [] || values[i] !== undefined, values[i] !== {}) {
+    // Search '1080' in 'Movies' category and limit to 20 results
+    let torrentSearchPromise = Promise.resolve(
+        torrentSearch.search(g_serverVariables.search_keywords, 'all', 20)
+        .then(results => {
+            let clean = [];
+            for (let r of results) {
+                clean.push({
+                    "title": r.title,
+                    "url": r.desc,
+                    "size": r.size,
+                    "seeds": r.seeds,
+                    "leechers": (!(isNaN(r.peers - r.seed)) &&
+                            (r.peers - r.seed > 0)) ?
+                        r.peers - r.seed : 0
+                });
+            }
+            return clean;
+        })
+        .catch(err => {
+            return err;
+        })
+    );
 
-        //         for (let torrent of values[i]) {
-        //             /**
-        //              * Filter duplicate titles.
-        //              */
-        //             if (!torrentTitleList.some(title => title === torrent.title)) {
-        //                 torrentTitleList.push(torrent.title);
-        //                 resultArray.push(torrent);
-        //             }
-        //         }
-
-        //     }
-        //     else {
-        //         console.log("error in value " + i);
-        //     }
-        // }
-        // console.log(resultArray);
-        // console.log(resultArray.length + " result(s) found.");
-
-    })
-    .catch(err => console.log(err));
+    Promise.all([torrentSearchPromise, PirateBayPromise /*, ExtraTorrentPromise*/ ]).then(values => {
+        let resultArray = [];
+        let torrentTitleList = [];
+        for (let i = 0; i < values.length -1; ++i) {
+            for (let torrent in values[i]) {
+                if (!torrentTitleList.some(title => title === values[i][torrent].title)) {
+                    torrentTitleList.push(values[i][torrent].title);
+                    resultArray.push(values[i][torrent]);
+                }
+            }
+        }
+        console.log(resultArray);
+        console.log(resultArray.length + " result(s) found.");
+        return resultArray;
+    }).then((value) => {res.json(value);}).catch(err => console.log(err));
+});
